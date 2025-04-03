7a74445c950e080a59493e137d3c76cbfb0bb719
zeta <jersson.z032@gmail.com>
2025-04-03 09:32:23 -0500

    agrego emmet configuracion

---
+++ lua/plugins/emmet.lua
@@ -0,0 +1,6 @@
+-- Instalar Emmet en Neovim usando lazy.nvim
+return {
+    "mattn/emmet-vim",
+    ft = { "html", "css", "javascript", "typescript" }, -- Activa Emmet para estos tipos de archivo
+  }
+  
